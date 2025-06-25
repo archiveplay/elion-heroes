1ed631dd96b3792fdf0816a5e823af74b95ca71b
Vladislav Nefedov <clay.zenx@gmail.com>
2025-06-25 17:06:01 +0300

    extend window with Telegram

---
+++ src/types/telegram.d.ts
@@ -0,0 +1,26 @@
+interface TelegramWebApp {
+  initData: string
+  initDataUnsafe: any
+  version: string
+  platform: string
+  close(): void
+  expand(): void
+  isExpanded: boolean
+  ready(): void
+  disableVerticalSwipes(): void
+  setHeaderColor(color: string): void
+  MainButton: {
+    setText(text: string): void
+    onClick(callback: () => void): void
+    show(): void
+    hide(): void
+  }
+}
+
+interface Window {
+  Telegram: {
+    WebApp: TelegramWebApp
+  }
+}
+
+
